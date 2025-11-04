761277c4fc87c007a8aaa530c942d0af62c6732a
Yegor Usoltsev <yegor.usoltsev@noom.com>
2025-11-04 15:29:46 +0300

    Add native external browser handler with fallback

--- SandboxApp.swift
+++ SandboxApp.swift
@@ -19,12 +19,26 @@ struct ContentView: View {
 struct WebView: UIViewRepresentable {
     let url: URL
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
     func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
+        let config = WKWebViewConfiguration()
+        config.userContentController.add(context.coordinator, name: "openExternalBrowser")
+        
+        let webView = WKWebView(frame: .zero, configuration: config)
         webView.allowsBackForwardNavigationGestures = true
         webView.load(URLRequest(url: url))
         return webView
     }
     
     func updateUIView(_ uiView: WKWebView, context: Context) {}
+    
+    class Coordinator: NSObject, WKScriptMessageHandler {
+        func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+            guard let urlString = message.body as? String, let url = URL(string: urlString) else { return }
+            UIApplication.shared.open(url)
+        }
+    }
 }
