0155cf898e0d085425e59003792795363e3bf79b
Yegor Usoltsev <yegor.usoltsev@noom.com>
2025-11-04 15:20:28 +0300

    Initial commit: Add index.html and SandboxApp.swift

---
+++ SandboxApp.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+import WebKit
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        WebView(url: URL(string: "https://www.google.com")!)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.allowsBackForwardNavigationGestures = true
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+}
